2af3713a077f02daa88128ffcc4f74d58dbef33c
polmf <99polmf@gmail.com>
2024-12-01 20:58:57 +0100

    millores_frontend

--- main_.py
+++ main_.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from ui_blocks import render_block
-from cbr_process import handle_cbr_process
 
 def main_nou_cas():
     st.title("Museum Visitor Questionnaire")
@@ -10,8 +9,8 @@ def main_nou_cas():
 
     render_block(st.session_state.block)
 
-    if st.session_state.block == 5:
-        handle_cbr_process()
+    if st.session_state.block == 6:
+        st.write("Thank you for completing the questionnaire!")
 
 if __name__ == "__main__":
     main_nou_cas()
