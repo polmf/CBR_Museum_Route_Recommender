7807b59f6525232d5480e889d86a09beb6d7e348
lola-monroy <lola.monroy@estudiantat.upc.edu>
2024-12-09 16:57:26 +0100

    clusteringg

--- artworks_data/clust_2.py
+++ artworks_data/clust_2.py
@@ -3,30 +3,44 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.decomposition import PCA
 
 data = pd.read_csv("C:/Users/lolam/Desktop/SBC_2/artworks_data/artworks_final.csv") # no va a ser necesario _4clustering
 
-features = ["Artist", "ConstituentID", "Date", "Medium", "Date", "Style"]
+features = ["Date", "Medium", "Style"]
 
-# codificar cols 
+
 le_style = LabelEncoder()
 data['Style_encoded'] = le_style.fit_transform(data['Style'])
-
-le_artist = LabelEncoder()
-data['Artist_encoded'] = le_artist.fit_transform(data['Artist'])
 
 le_medium = LabelEncoder()
 data['Medium_encoded'] = le_medium.fit_transform(data['Medium'])
 
-clustering_data = data[['Dim_cm2', 'Date', 'Style_encoded', 'Artist_encoded', 'Medium_encoded']].dropna()
+# Crear un nuevo dataframe con las características para clustering
+clustering_data = data[[ 'Date', 'Style_encoded','Medium_encoded']].dropna()
 
-# normalitzem 
+# Normalizar las características
 scaler = StandardScaler()
 clustering_data_scaled = scaler.fit_transform(clustering_data)
 
-kmeans = KMeans(n_clusters=5, random_state=42)  # Cambia n_clusters según el análisis
+# Determinar el número óptimo de clusters usando el método del codo
+inertia = []
+k_range = range(1, 11)
+for k in k_range:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(clustering_data_scaled)
+    inertia.append(kmeans.inertia_)
+
+# # descomentar para visualizar el método del codo
+# plt.figure(figsize=(8, 5))
+# plt.plot(k_range, inertia, marker='o')
+# plt.title("Método del Codo para Determinar el Número de Clusters (para clust_2)")
+# plt.xlabel("Número de Clusters")
+# plt.ylabel("Inercia")
+# plt.show()
+
+# número óptimo de clusters --> 4
+kmeans = KMeans(n_clusters=4, random_state=42)  
 clustering_data['Cluster'] = kmeans.fit_predict(clustering_data_scaled)
 
 # Reducir a 2 dimensiones para la visualización con PCA
@@ -45,7 +59,7 @@ sns.scatterplot(
     hue=clustering_data['Cluster'],
     palette='viridis'
 )
-plt.title("Clusters de Obras de Arte (PCA)")
+plt.title("Clusters de Obras de Arte (PCA) (para features style, medium y date)")
 plt.xlabel("PCA1")
 plt.ylabel("PCA2")
 plt.legend(title="Cluster")
@@ -56,4 +70,4 @@ cluster_summary = clustering_data.groupby('Cluster').mean()
 print(cluster_summary)
 
 # Guardar los resultados
-# clustering_data.to_csv("artworks_clustered.csv", index=False)
+clustering_data.to_csv("artworks_clustered.csv", index=False)
